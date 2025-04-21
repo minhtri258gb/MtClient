16820c43aa4b726135658529e8667a09e12c5102
minhtri258gb <minhtri258em@gmail.com>
2025-04-21 22:39:38 +0700

    [Anime] update

--- anime/script.js
+++ anime/script.js
@@ -91,14 +91,14 @@ var mt = {
 				{ title: "STT", formatter: 'rownum', width: 40, hozAlign: 'center', headerSort: false },
 				// { title: "Image", field: 'image_url', formatter: 'image', width: 100, formatterParams: { height: '100px', width: '100px', class: 'anime-image' }},
 				{ title: "Name", field: 'name', headerSort: false, editor: 'input' },
-				{ title: "Story", field: 'story', width: 69, ...ratingProp},
-				{ title: "Art", field: 'art', width: 56, ...ratingProp},
-				{ title: "Sound", field: 'sound', width: 82, ...ratingProp},
-				{ title: "Fantasy", field: 'fantasy', width: 84, ...ratingProp},
-				{ title: "Sad", field: 'sad', width: 57, ...ratingProp},
-				{ title: "Joke", field: 'joke', width: 63, ...ratingProp},
-				{ title: "Brand", field: 'brand', width: 72, ...ratingProp},
-				{ title: "Review", field: 'review', width: 81, ...ratingProp},
+				{ title: "Story", field: 'story', width: 70, ...ratingProp},
+				{ title: "Art", field: 'art', width: 70, ...ratingProp},
+				{ title: "Sound", field: 'sound', width: 70, ...ratingProp},
+				{ title: "Fantasy", field: 'fantasy', width: 70, ...ratingProp},
+				{ title: "Sad", field: 'sad', width: 70, ...ratingProp},
+				{ title: "Joke", field: 'joke', width: 70, ...ratingProp},
+				{ title: "Brand", field: 'brand', width: 70, ...ratingProp},
+				{ title: "Review", field: 'review', width: 70, ...ratingProp},
 				{ title: "End", field: 'end', width: 80, headerSort: false, editor: 'input'},
 				{ title: "Character", field: 'character', width: 100, headerSort: false, editor: 'input'},
 				{ title: "Time", field: 'updateTime', width: 143},
@@ -116,6 +116,13 @@ var mt = {
 		// Register Event
 		this.c_table.on('cellEdited', (cell) => this.cellEdited(cell));
 		this.c_table.on('cellEditCancelled', (cell) => this.cellEditCancelled(cell));
+
+		// Init Modal
+		MicroModal.init({
+			// openTrigger: '',
+			disableFocus: true,
+			debugMode: true,
+		});
 	},
 	rowContextMenu: function(event, row) {
 		let actions = [];
@@ -123,21 +130,33 @@ var mt = {
 		let rowData = row.getData();
 
 		// Lưu
-		if (rowData.isEdit) {
+		if (rowData.id == -1 || rowData._origin != null) {
 			actions.push({
 				label: '<img class="menuIcon" src="/res/icons/save16.png" />Save',
-				action: (e, row) => {
-					row.delete();
-				}
+				action: (e, row) => this.actionSave(row),
+			});
+		}
+
+		// Đặt lại như cũ
+		if (rowData._origin != null) {
+			actions.push({
+				label: '<img class="menuIcon" src="/res/icons/revert16.png" />Revert',
+				action: (e, row) => this.actionRevert(row),
+			});
+		}
+
+		// Bỏ thêm mới
+		if (rowData.id == -1) {
+			actions.push({
+				label: '<img class="menuIcon" src="/res/icons/remove16.png" />Remove',
+				action: (e, row) => this.actionRemove(row),
 			});
 		}
 
 		// Tạo mới
 		actions.push({
 			label: '<img class="menuIcon" src="/res/icons/add.png" />New',
-			action: (e, row) => {
-				row.delete();
-			}
+			action: (e, row) => this.actionAdd(row),
 		});
 
 		// Action bảng
@@ -158,26 +177,113 @@ var mt = {
 		return actions;
 	},
 	cellEdited: function(cell) { // Sau khi sửa
-		// var oldValue = cell.getOldValue(); // Giá trị trước khi chỉnh sửa
-		// var newValue = cell.getValue(); // Giá trị sau khi chỉnh sửa
-		// var field = cell.getField(); // Tên cột (field)
+
 		var row = cell.getRow(); // Cột đã chỉnh sửa
 		let rowData = row.getData(); // Dữ liệu của hàng
 
-		// console.log("Ô đã thay đổi:");
-		// console.log("Cột: ", field);
-		// console.log("Giá trị cũ: ", oldValue);
-		// console.log("Giá trị mới: ", newValue);
-		// console.log("Dữ liệu hàng: ", rowData);
+		// Bỏ qua nếu là thêm mới
+		if (rowData.id == -1)
+			return;
 
 		// Update row
-		rowData.isEdit = true;
+		if (rowData._origin == null) {
 
-		// Hightlight row
-		row.getElement().classList.add('highlight-row');
+			var oldValue = cell.getOldValue(); // Giá trị trước khi chỉnh sửa
+			var field = cell.getField(); // Tên cột (field)
+
+			// Backup data
+			let origin = Object.assign({}, rowData);
+			origin[field] = oldValue;
+			rowData._origin = origin;
+
+			// Hightlight row
+			row.getElement().classList.add('highlight-row-edit');
+		}
 	},
 	cellEditCancelled: function(cell) { // Hủy thay đổi
 		console.log('cellEditCancelled', cell);
 	},
+	actionAdd: function(row) {
+		this.c_table.addData([{ id: -1}], true)
+			.then((row) => {
+
+				// Hightlight row sau khi thêm mới
+				row[0].getElement().classList.add('highlight-row-add');
+			});
+	},
+	actionSave: function(row) {
+
+		// Lấy dữ liệu của hàng
+		let rowData = row.getData();
+
+		// Cập nhật thời gian
+		let curdate = new Date();
+		const year = curdate.getFullYear();
+		const month = String(curdate.getMonth() + 1).padStart(2, '0');
+		const day = String(curdate.getDate()).padStart(2, '0');
+		const hours = String(curdate.getHours()).padStart(2, '0');
+		const minutes = String(curdate.getMinutes()).padStart(2, '0');
+		const seconds = String(curdate.getSeconds()).padStart(2, '0');
+		rowData.updateTime = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+
+		let sql = '';
+		let action = '';
+		if (rowData.id == -1) {
+			action = 'INSERT';
+			let lstKey = [];
+			let lstValue = [];
+			for (let key in rowData) {
+
+				// Bỏ qua qua các trường không cần lưu
+				if (key == 'id' || key == '_origin')
+					continue;
+
+				let value = rowData[key];
+
+				// Nếu kiểu chuỗi và có giá trị thì bọc trong nháy đơn
+				if (typeof value == 'string' && value.length > 0)
+					value = `'${value}'`;
+
+				// Check NULL
+				if (value == null || value == '')
+					value = 'NULL';
+
+				lstKey.push(key);
+				lstValue.push(value);
+			}
+			sql += `INSERT INTO anime (${lstKey.join(', ')})\n`;
+			sql += `VALUES (${lstValue.join(', ')});`;
+		}
+		else if (rowData._origin != null) {
+			action = 'UPDATE';
+			
+		}
+
+		// Replace SQL
+		let htmlSQL = Prism.highlight(sql, Prism.languages.sql, 'sql');
+		// let htmlSQL = sql.replace(/\n/g, '<br>');
+		$('#modal-1-title').html(action+ ' SQL');
+		$('#sqlcontent').html(htmlSQL);
+
+		// Show Confirm SQL
+		MicroModal.show('modal-1');
+	},
+	actionRevert: function(row) {
+		let rowData = row.getData();
+		let origin = rowData._origin;
+		if (origin != null) {
+
+			// Cập nhật lại data cũ
+			origin._origin = null;
+			row.update(origin);
+
+			// Bỏ Hightlight row
+			row.getElement().classList.remove('highlight-row-edit');
+		}
+	},
+	actionRemove: function(row) {
+		row.delete();
+	},
+
 };
 $(document).ready(() => mt.init());
