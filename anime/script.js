f19f0ffec97b7cabf1fe84bfd45f66f400556934
trinm_hinnova <trinm@hinnova.com.vn>
2025-04-22 16:27:43 +0700

    [Anime] update prismjs sql

--- anime/script.js
+++ anime/script.js
@@ -1,5 +1,6 @@
 
 import mtAuthen from '/common/authen.js';
+import mtUtil from '/common/util.js';
 
 var mt = {
 	// h_endpoint: 'https://api.jikan.moe/v4/top/anime',
@@ -7,6 +8,8 @@ var mt = {
 	p_authen: mtAuthen,
 	c_table: null,
 	m_database: 'manager',
+	d_row: '', // Row đang lưu
+	d_sql: '', // SQL
 
 	// Method
 	init: async function() {
@@ -35,6 +38,7 @@ var mt = {
 			},
 			editor: 'number',
 			editorParams: { min: 1, max: 5, step: 1 },
+			editable: false,
 		};
 
 		// Init Tabulator
@@ -57,7 +61,7 @@ var mt = {
 				body: (url, config, params) => {
 					return JSON.stringify({
 						database: this.m_database,
-						select: 'name,story,art,sound,fantasy,sad,joke,brand,review,end,character,updateTime',
+						select: 'id,img,name,story,art,sound,fantasy,sad,joke,brand,review,end,character,time',
 						from: 'anime',
 						where: '',
 						sort: 'time DESC',
@@ -74,23 +78,32 @@ var mt = {
 			// 		sort: "desc",
 			// 	};
 			// },
-			// ajaxResponse: function (url, params, response) {
-			// 	let listData = response.data.map(item => ({
-			// 		image_url: item.images.jpg.image_url,
-			// 		title: item.title,
-			// 		synopsis: item.synopsis ? item.synopsis.substring(0, 200) + "..." : "No synopsis available",
-			// 		type: item.type,
-			// 		episodes: item.episodes || "N/A"
-			// 	}));
-			// 	return {
-			// 		data: listData,
-			// 		last_page: response.pagination.last_visible_page
-			// 	};
-			// },
+			ajaxResponse: function (url, params, response) {
+				for (let row of response.data) {
+					if (row.img == null || row.img == '') {
+						row.img = '/res/icons/image16.png';
+					}
+				}
+				// let listData = response.data.map(item => ({
+				// 	image_url: item.images.jpg.image_url,
+				// 	title: item.title,
+				// 	synopsis: item.synopsis ? item.synopsis.substring(0, 200) + "..." : "No synopsis available",
+				// 	type: item.type,
+				// 	episodes: item.episodes || "N/A"
+				// }));
+				// return {
+				// 	data: listData,
+				// 	last_page: response.pagination.last_visible_page
+				// };
+				return response;
+			},
 			columns: [
 				{ title: "STT", formatter: 'rownum', width: 40, hozAlign: 'center', headerSort: false },
-				// { title: "Image", field: 'image_url', formatter: 'image', width: 100, formatterParams: { height: '100px', width: '100px', class: 'anime-image' }},
-				{ title: "Name", field: 'name', headerSort: false, editor: 'input' },
+				{ title: "Image", field: 'img', formatter: 'image', width: 80, visible: false
+					, formatterParams: { height: '100px', width: '70px', class: 'anime-image' }
+					, editor: 'input', editable: false
+				},
+				{ title: "Name", field: 'name', headerSort: false, editor: 'input', editable: false },
 				{ title: "Story", field: 'story', width: 70, ...ratingProp},
 				{ title: "Art", field: 'art', width: 70, ...ratingProp},
 				{ title: "Sound", field: 'sound', width: 70, ...ratingProp},
@@ -99,9 +112,14 @@ var mt = {
 				{ title: "Joke", field: 'joke', width: 70, ...ratingProp},
 				{ title: "Brand", field: 'brand', width: 70, ...ratingProp},
 				{ title: "Review", field: 'review', width: 70, ...ratingProp},
-				{ title: "End", field: 'end', width: 80, headerSort: false, editor: 'input'},
-				{ title: "Character", field: 'character', width: 100, headerSort: false, editor: 'input'},
-				{ title: "Time", field: 'updateTime', width: 143},
+				{ title: "End", field: 'end', width: 80, headerSort: false, editor: 'input', editable: false},
+				{ title: "Character", field: 'character', width: 100, headerSort: false, editor: 'input', editable: false},
+				{ title: "Time", field: 'time', width: 69, formatter: (cell) => {
+					let timestamp = cell.getValue();
+					if (timestamp == null)
+						return ''
+					return mtUtil.date.date_to_str(new Date(timestamp * 1000), 'dd/MM/yy');
+				} },
 			],
 			rowContextMenu: (event, row) => this.rowContextMenu(event, row),
 			headerSortElement: function(column, dir) {
@@ -115,7 +133,7 @@ var mt = {
 
 		// Register Event
 		this.c_table.on('cellEdited', (cell) => this.cellEdited(cell));
-		this.c_table.on('cellEditCancelled', (cell) => this.cellEditCancelled(cell));
+		this.c_table.on('cellDblClick', (event, cell) => this.cellDblClick(event, cell));
 
 		// Init Modal
 		MicroModal.init({
@@ -162,12 +180,13 @@ var mt = {
 		// Action bảng
 		actions.push(
 			{
-				label: '<img class="menuIcon" src="/res/icons/eye16.png" />Show',
+				label: '<img class="menuIcon" src="/res/icons/eye16.png" />Columns',
 				menu: [
 					{
 						label: '<img class="menuIcon" src="/res/icons/image16.png" />Image',
-						action: (e, row) => {
-							row.delete();
+						action: (e, row) => { 
+							this.c_table.toggleColumn('img');
+							this.c_table.redraw(true);
 						}
 					},
 				]
@@ -178,18 +197,26 @@ var mt = {
 	},
 	cellEdited: function(cell) { // Sau khi sửa
 
-		var row = cell.getRow(); // Cột đã chỉnh sửa
+		let row = cell.getRow(); // Cột đã chỉnh sửa
 		let rowData = row.getData(); // Dữ liệu của hàng
 
 		// Bỏ qua nếu là thêm mới
 		if (rowData.id == -1)
 			return;
 
+		let oldValue = cell.getOldValue(); // Giá trị trước khi chỉnh sửa
+		let field = cell.getField(); // Tên cột (field)
+		let value = rowData[field];
+
+		// Normalize data
+		if (oldValue == '') oldValue = null;
+		if (value == '') value = null;
+		if (oldValue == value)
+			return; // Xem như chưa thay đổi
+
 		// Update row
 		if (rowData._origin == null) {
 
-			var oldValue = cell.getOldValue(); // Giá trị trước khi chỉnh sửa
-			var field = cell.getField(); // Tên cột (field)
 
 			// Backup data
 			let origin = Object.assign({}, rowData);
@@ -200,8 +227,8 @@ var mt = {
 			row.getElement().classList.add('highlight-row-edit');
 		}
 	},
-	cellEditCancelled: function(cell) { // Hủy thay đổi
-		console.log('cellEditCancelled', cell);
+	cellDblClick: function(event, cell) { // Open Editor
+		cell.edit(true);
 	},
 	actionAdd: function(row) {
 		this.c_table.addData([{ id: -1}], true)
@@ -217,19 +244,15 @@ var mt = {
 		let rowData = row.getData();
 
 		// Cập nhật thời gian
-		let curdate = new Date();
-		const year = curdate.getFullYear();
-		const month = String(curdate.getMonth() + 1).padStart(2, '0');
-		const day = String(curdate.getDate()).padStart(2, '0');
-		const hours = String(curdate.getHours()).padStart(2, '0');
-		const minutes = String(curdate.getMinutes()).padStart(2, '0');
-		const seconds = String(curdate.getSeconds()).padStart(2, '0');
-		rowData.updateTime = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+		// let curdate = new Date();
+		// rowData.updateTime = mtUtil.date.date_to_str(curdate);
+		rowData.time = Math.floor(Date.now() / 1000);
 
 		let sql = '';
 		let action = '';
 		if (rowData.id == -1) {
 			action = 'INSERT';
+
 			let lstKey = [];
 			let lstValue = [];
 			for (let key in rowData) {
@@ -256,17 +279,51 @@ var mt = {
 		}
 		else if (rowData._origin != null) {
 			action = 'UPDATE';
-			
+
+			let origin = rowData._origin;
+			for (let key in rowData) {
+
+				// Bỏ qua qua các trường không cần lưu
+				if (key == 'id' || key == '_origin')
+					continue;
+
+				let value = rowData[key];
+
+				// Nếu không thay đổi thì bỏ qua
+				if (value === origin[key])
+					continue
+
+				// Nếu kiểu chuỗi và có giá trị thì bọc trong nháy đơn
+				if (typeof value == 'string' && value.length > 0)
+					value = `'${value}'`;
+
+				// Check NULL
+				if (value == null || value == '')
+					value = 'NULL';
+
+				sql += `,\n\t${key} = ${value}`;
+			}
+			sql = 'UPDATE anime\nSET ' + sql.substring(3) + '\nWHERE id = ' + rowData.id + ';';
 		}
 
 		// Replace SQL
-		let htmlSQL = Prism.highlight(sql, Prism.languages.sql, 'sql');
+		// let htmlSQL = Prism.highlight(sql, Prism.languages.sql, 'sql');
 		// let htmlSQL = sql.replace(/\n/g, '<br>');
 		$('#modal-1-title').html(action+ ' SQL');
-		$('#sqlcontent').html(htmlSQL);
+
+		let $codeBlock = $('#codeBlock');
+		// $codeBlock.html(htmlSQL);
+		$codeBlock.text(sql);
+
+		// Highlight Code
+		Prism.highlightElement($codeBlock[0]);
 
 		// Show Confirm SQL
 		MicroModal.show('modal-1');
+
+		// Save data var
+		this.d_row = row;
+		this.d_sql = sql;
 	},
 	actionRevert: function(row) {
 		let rowData = row.getData();
@@ -284,6 +341,25 @@ var mt = {
 	actionRemove: function(row) {
 		row.delete();
 	},
+	actionExeccute: function() {
+
+		console.log('actionExeccute', this.d_sql);
+
+		// Call API - Exec Database
+
+		// Xóa bỏ origin
+		let rowData = this.d_row.getData();
+		rowData._origin = null;
+		this.d_row.update(rowData);
+
+		// Đóng modal
+		MicroModal.close('modal-1');
+
+		// Tắt highlight sau khi lưu
+		let highlightClass = 'highlight-row-' + ((this.d_sql.startsWith('INSERT')) ? 'add' : 'edit');
+		this.d_row.getElement().classList.remove(highlightClass);
+
+	}
 
 };
 $(document).ready(() => mt.init());
--- common/util.js
+++ common/util.js
@@ -5,4 +5,43 @@ var mtUtil = {
 		console.log(object);
 		console.groupEnd();
 	},
+
+	// DataType
+	date: {
+		date_to_str(date, format) { // Chuyển date thành string format dd/MM/yyyy HH:mm:ss
+			if (format == null || format.length == '')
+				format = 'dd/MM/yyyy HH:mm:ss';
+
+			let strDate = format;
+
+			// Day
+			if (format.indexOf('dd') > -1)
+				strDate = strDate.replace('dd', String(date.getDate()).padStart(2, '0'));
+
+			// Month
+			if (format.indexOf('MM') > -1)
+				strDate = strDate.replace('MM', String(date.getMonth() + 1).padStart(2, '0'));
+
+			// Year
+			if (format.indexOf('yyyy') > -1)
+				strDate = strDate.replace('yyyy', date.getFullYear());
+			else if (format.indexOf('yy') > -1)
+				strDate = strDate.replace('yy', date.getFullYear() % 100);
+
+			// Hour
+			if (format.indexOf('HH') > -1)
+				strDate = strDate.replace('HH', String(date.getHours()).padStart(2, '0'));
+
+			// Minute
+			if (format.indexOf('mm') > -1)
+				strDate = strDate.replace('mm', String(date.getMinutes()).padStart(2, '0'));
+
+			// Second
+			if (format.indexOf('ss') > -1)
+				strDate = strDate.replace('ss', String(date.getSeconds()).padStart(2, '0'));
+
+			return strDate;
+		}
+	},
 };
+export default mtUtil;
